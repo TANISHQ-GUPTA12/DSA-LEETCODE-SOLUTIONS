89ca90eecb440eb7bc3af56f69cb800a9ecc4b7c
DEADPOOL_12 <111686997+TANISHQ-GUPTA12@users.noreply.github.com>
2023-09-26 18:28:03 +0530

    Time: 95 ms (54.97%), Space: 93.6 MB (14.83%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int min=prices[0];
+        int mini=prices[0];
         
         int ind1=0;
         int ind2=0;
         for(int i=0;i<prices.size();i++)
         {
-           if(prices[i]<min)
-           {
-               min=prices[i];
-           }
-            ind1= prices[i]-min;
-            ind2=max(ind2,ind1);
+               mini=min(mini,prices[i]); 
+            ind2=max(ind2,prices[i]-mini);
         }
         return ind2;
     }
